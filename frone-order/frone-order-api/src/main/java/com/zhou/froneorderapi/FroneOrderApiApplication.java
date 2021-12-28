176445527e6bfa5b5913152461540d3a2539c186
zhoumingbing <zhouzhuang2017@163.com>
2021-12-29 07:54:30 +0800

    Initial commit

---
+++ frone-order/frone-order-api/src/main/java/com/zhou/froneorderapi/FroneOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.zhou.froneorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FroneOrderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FroneOrderApiApplication.class, args);
+    }
+
+}
---
+++ frone-order/frone-order-api/src/test/java/com/zhou/froneorderapi/FroneOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhou.froneorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FroneOrderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
